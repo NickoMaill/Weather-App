4f8fcd78fedc5431193c87d43edb1254c65a9d67
Nicko <nickos@iMac.local>
2022-03-20 11:18:42 +0100

    set server base

--- Back/server.js
+++ Back/server.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+	res.send("Bonjour");
+});
+
+app.get("*", (_req, res) => {
+    res.status(404).send("Page not found");
+});
+
+app.listen(PORT, () => console.log("Listening on port 8000"));
