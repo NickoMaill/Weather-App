2c3162cbe70786845a88b78e297ecc1375b7f823
Nicko <nickos@iMac.local>
2022-03-20 11:21:10 +0100

    add cors middleware (without library)

--- Back/server.js
+++ Back/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const cors = require("./Middlewares/cors");
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(express.static("public"));
+app.use(cors)
 
 app.get("/", (_req, res) => {
 	res.send("Bonjour");
 });
 
 app.get("*", (_req, res) => {
-    res.status(404).send("Page not found");
+	res.status(404).send("Page not found");
 });
 
 app.listen(PORT, () => console.log("Listening on port 8000"));
