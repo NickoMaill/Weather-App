6ebac6f4841eed2fb3e00cef7189a63eca0c303a
Nicko <nickos@iMac.local>
2022-04-07 18:54:57 +0200

    set React native clean blank

---
+++ Front/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Route, Routes } from "react-router-native";
+import Favorites from "./views/Favorites";
+import Homepage from "./views/Homepage";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
+export default function App() {
+	return (
+		<NativeRouter>
+			<Routes>
+				<Route exact path="/" element={<Login />} />
+				<Route exact path="/homepage" element={<Homepage />} />
+				<Route exact path="/favorites" element={<Favorites />} />
+				<Route exact path="/register" element={<Register />} />
+			</Routes>
+		</NativeRouter>
+    
+	);
+}
+
+const styles = StyleSheet.create({});
---
+++ Front/views/Homepage.js
@@ -0,0 +1,13 @@
+
+import { Text } from "react-native";
+import { SafeAreaView, View } from "react-native";
+
+export default function Homepage() {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text></Text>
+        </View>
+    </SafeAreaView>
+  )
+}
